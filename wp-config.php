1fe865b95759f48a3c3d360035d66579e1146c73
Hawk Nguyen <verynoob189@gmail.com>
2019-03-03 17:19:18 -0500

    update wp

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,8 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', 'C:\wamp64\www\studentloan-v2\wp-content\plugins\wp-super-cache/' );
 define( 'DB_NAME', 'test_db' );
 
 /** MySQL database username */
